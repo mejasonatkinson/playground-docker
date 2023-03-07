c9bcc4817edb4b1dece7d293bbf2fc8aa2e27c26
mejasonatkinson <me@jasonatkinson.co.uk>
2023-03-07 07:40:15 +0000

    project wordpress with docker-compose 4

---
+++ projects/wordpress-with-docker-compose-4/wordpress/plugins/demo-plugin/demo-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: demo-plugin
+* Plugin URI: http://demo-plugin.com
+* Description: A description about the plugin
+* Author: Author Name
+* Author URI: http://www.author.com
+* Version: 0.0.1
+**/
+?>
